be751ee1b39d597991b5c497dd5898a23ff47ed4
Yesid-Ackerman <deironyesithym21@gmail.com>
2024-06-19 14:45:34 -0500

    Pagina despues de login con link a funcionalidades

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-2">
+            <div class="sidebar">
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('libraries.index') }}">Librerías</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('books.index') }}">Libros</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('shelvings.index') }}">Estantes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('topics.index') }}">Temas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('authors.index') }}">Autores</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-10">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
